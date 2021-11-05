79ce709c2e5cfbe3db01c1fea645752bc9991ed8
zoemin2 <yzoemin243@gmail.com>
2021-11-05 21:29:57 +0800

    Add in sample queries

--- data/q5.sql
+++ data/q5.sql
@@ -1,7 +1,24 @@
-SELECT  
-    year7079.cnt as yr7079, 
-    year8089.cnt as yr8089, 
-    year9099.cnt as yr9099, 
-    year0009.cnt as yr0009, 
-    year1019.cnt as yr1019
-FROM    year7079, year8089, year9099, year0009, year1019;
+SELECT
+    n_name,
+    sum(l_extendedprice * (1 - l_discount)) as revenue
+FROM
+    customer,
+    orders,
+    lineitem,
+    supplier,
+    nation,
+    region
+WHERE
+    c_custkey = o_custkey
+    AND l_orderkey = o_orderkey
+    AND l_suppkey = s_suppkey
+    AND c_nationkey = s_nationkey
+    AND s_nationkey = n_nationkey
+    AND n_regionkey = r_regionkey
+    AND r_name = 'ASIA'
+    AND o_orderdate >= date '1994-01-01'
+    AND o_orderdate < date '1994-01-01' + interval '1' year
+GROUP BY
+    n_name
+ORDER BY
+    revenue desc;
