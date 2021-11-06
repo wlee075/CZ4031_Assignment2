8d0ee80eaccfbac464ddfda7dedd22cbd08f7d37
Zheng Tao <zhengtaolim@gmail.com>
2021-11-07 04:21:13 +0800

    Shift explain_query code into interface

--- Interface.py
+++ Interface.py
@@ -2,15 +2,17 @@
 Application for query interface (Windows Compatible)
 """
 import json
+import json
+import logging
+import psycopg2
 
+from algorithms.Annotation import parse_plan
 try:
     # python 2.x
     from Tkinter import *
 except ImportError:
     # python 3.x
     from tkinter import *
-
-from query_executor.explain_query import Explain
 
 class InterfaceApp(Tk):
     """ App class for Query Plan """
@@ -130,3 +132,83 @@ class InterfaceApp(Tk):
         parsed_plan = self.explanator.parse(query_plan)
         self.entry_parsed_plan.delete("1.0", END)
         self.entry_parsed_plan.insert("1.0", json.dumps(parsed_plan, indent=4))
+
+class Explain:
+    """ Class to explain query """
+    def __init__(
+            self, host, port, dbname, user, password,
+            desc=True, voice=False, debug=False
+        ):
+        """ init Explain """
+        conn_string = "host='%s' port='%s' dbname='%s' user='%s' password='%s'"%(
+            host, port, dbname, user, password)
+        self.conn = psycopg2.connect(conn_string)
+        self.cursor = self.conn.cursor()
+        
+        logging.info("Connected to database: " + conn_string)
+
+        self.desc = desc
+        self.debug = debug
+
+        self.query = ""
+        self.query_plan = {}
+        self.parsed_plan = ""
+
+
+    def explain(self, query=None):
+        """ explain query """
+        if query:
+            self.query = query
+
+        logging.info("Generating query plan for: " + self.query)
+        try:
+            self.cursor.execute("EXPLAIN (FORMAT JSON) " + self.query)
+            plan = self.cursor.fetchall()
+            self.query_plan = plan[0][0][0]["Plan"]
+        except:
+            logging.error("Generate query plan execution failed")
+            self.query_plan = {}
+            self.parsed_plan = "Generate query plan execution failed"
+            raise
+        finally:
+            logging.info("Generated query plan: " + json.dumps(self.query_plan, indent=4))
+
+        return self.query_plan
+        
+    def loop_explain(self):
+        """ continuously explain queries """
+        print("postgres=# Please input query (end with ';')")
+        next_line = ""
+        self.query = ""
+        while next_line.strip().lower() != "quit":
+            next_line = input("postgres=# ")
+            self.query += "\n" + next_line.strip()
+            if self.query[-1] == ";":
+                try:
+                    self.explain()
+                    self.parse()
+                except Exception as exception:
+                    logging.error("Error for Explain.explain(): " + str(exception))
+                finally:
+                    if self.debug:
+                        print(json.dumps(self.query_plan, indent=4))
+                    if self.desc:
+                        print(self.parsed_plan)
+                    self.query = ""
+    
+    def parse(self, query_plan=None):
+        """ Parse query plan """
+        if query_plan:
+            self.query_plan = query_plan
+
+        logging.info("Parsing plan: " + json.dumps(self.query_plan, indent=4))
+        try:
+            self.parsed_plan = parse_plan(self.query_plan, start=True)
+        except:
+            logging.error("Parse query plan execution failed")
+            self.parsed_plan = "Parse query plan execution failed"
+            raise
+        finally:
+            logging.info("Parsed plan: " + self.parsed_plan)
+
+        return self.parsed_plan
