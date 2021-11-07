70390a761c8ceacf56f2eb7b3f3d3d28c912362d
simin233 <70477778+simin233@users.noreply.github.com>
2021-11-07 21:47:21 +0800

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -76,42 +76,42 @@ class InterfaceApp(Tk):
 
         '''button for queries'''
         self.button_set = Button(
-            self.entry_query, text="q1",
+            self.frame_query, text="q1",
             width=5,command=self.q1_plan)
         self.button_set.pack(side=RIGHT)
 
         self.button_set = Button(
-            self.entry_query, text="q10",
+            self.frame_query, text="q10",
             width=5,command=self.q10_plan)
         self.button_set.pack(side=RIGHT)
         
         self.button_set = Button(
-            self.entry_query, text="q12",
+            self.frame_query, text="q12",
             width=5,command=self.q12_plan)
         self.button_set.pack(side=RIGHT)
 
         self.button_set = Button(
-            self.entry_query, text="q14",
+            self.frame_query, text="q14",
             width=5,command=self.q14_plan)
         self.button_set.pack(side=RIGHT)
 
         self.button_set = Button(
-            self.entry_query, text="q19",
+            self.frame_query, text="q19",
             width=5,command=self.q19_plan)
         self.button_set.pack(side=RIGHT)
 
         self.button_set = Button(
-            self.entry_query, text="q3",
+            self.frame_query, text="q3",
             width=5,command=self.q3_plan)
         self.button_set.pack(side=RIGHT)
 
         self.button_set = Button(
-            self.entry_query, text="q5",
+            self.frame_query, text="q5",
             width=5,command=self.q5_plan)
         self.button_set.pack(side=RIGHT)
 
         self.button_set = Button(
-            self.entry_query, text="q6",
+            self.frame_query, text="q6",
             width=5,command=self.q6_plan)
         self.button_set.pack(side=RIGHT)
         
@@ -155,38 +155,38 @@ class InterfaceApp(Tk):
 
 
     def q1_plan(self):
-        self.entry_plan.delete("1.0", END)
-        self.entry_plan.insert("1.0","SELECT l_returnflag,l_linestatus,sum(l_quantity) as sum_qty,sum(l_extendedprice) as sum_base_price,sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,avg(l_quantity) as avg_qty,avg(l_extendedprice) as avg_price,avg(l_discount) as avg_disc,count(*) as count_order FROM lineitem WHERE 1_shipdate <= date '1998-12-01' - interval '90' day GROUP BY 1_returnflag,l_linestatus ORDER BY l_returnflag,l_linestatus;")
+        self.entry_query.delete("1.0", END)
+        self.entry_query.insert("1.0","SELECT l_returnflag,l_linestatus,sum(l_quantity) as sum_qty,sum(l_extendedprice) as sum_base_price,sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,avg(l_quantity) as avg_qty,avg(l_extendedprice) as avg_price,avg(l_discount) as avg_disc,count(*) as count_order FROM lineitem WHERE 1_shipdate <= date '1998-12-01' - interval '90' day GROUP BY 1_returnflag,l_linestatus ORDER BY l_returnflag,l_linestatus;")
 
     def q10_plan(self):
-        self.entry_plan.delete("1.0", END)
-        self.entry_plan.insert("1.0","SELECT c_custkey,c_name,sum(l_extendedprice * (1 - l_discount)) as revenue,c_acctbal,n_name,c_address,c_phone,c_comment FROM customer,orders,lineitem,nation WHERE c_custkey = o_custkey AND l_orderkey = o_orderkey AND o_orderdate >= date '1993-10-01' AND o_orderdate < date '1993-10-01' + interval '3' month AND l_returnflag = 'R' AND c_nationkey = n_nationkey GROUP BY c_custkey,c_name,c_acctbal,c_phone,n_name,c_address,c_comment ORDER BY revenue desc LIMIT 20;")
+        self.entry_query.delete("1.0", END)
+        self.entry_query.insert("1.0","SELECT c_custkey,c_name,sum(l_extendedprice * (1 - l_discount)) as revenue,c_acctbal,n_name,c_address,c_phone,c_comment FROM customer,orders,lineitem,nation WHERE c_custkey = o_custkey AND l_orderkey = o_orderkey AND o_orderdate >= date '1993-10-01' AND o_orderdate < date '1993-10-01' + interval '3' month AND l_returnflag = 'R' AND c_nationkey = n_nationkey GROUP BY c_custkey,c_name,c_acctbal,c_phone,n_name,c_address,c_comment ORDER BY revenue desc LIMIT 20;")
 
 
     def q12_plan(self):
-        self.entry_plan.delete("1.0", END)
-        self.entry_plan.insert("1.0","SELECT l_shipmode,sum(case when o_orderpriority = '1-URGENT' OR o_orderpriority = '2-HIGH' then 1 else 0 end) as high_line_count,sum(case when o_orderpriority <> '1-URGENT' AND o_orderpriority <> '2-HIGH' then 1 else 0 end) AS low_line_count FROM orders,lineitem WHERE o_orderkey = l_orderkey AND l_shipmode in ('MAIL', 'SHIP') AND l_commitdate < l_receiptdate AND l_shipdate < l_commitdate AND l_receiptdate >= date '1994-01-01' AND l_receiptdate < date '1994-01-01' + interval '1' year GROUP BY l_shipmode ORDER BY l_shipmode;")
+        self.entry_query.delete("1.0", END)
+        self.entry_query.insert("1.0","SELECT l_shipmode,sum(case when o_orderpriority = '1-URGENT' OR o_orderpriority = '2-HIGH' then 1 else 0 end) as high_line_count,sum(case when o_orderpriority <> '1-URGENT' AND o_orderpriority <> '2-HIGH' then 1 else 0 end) AS low_line_count FROM orders,lineitem WHERE o_orderkey = l_orderkey AND l_shipmode in ('MAIL', 'SHIP') AND l_commitdate < l_receiptdate AND l_shipdate < l_commitdate AND l_receiptdate >= date '1994-01-01' AND l_receiptdate < date '1994-01-01' + interval '1' year GROUP BY l_shipmode ORDER BY l_shipmode;")
 
     def q14_plan(self):
-        self.entry_plan.delete("1.0", END)
-        self.entry_plan.insert("1.0","SELECT 100.00 * sum(case when p_type like 'PROMO%' then l_extendedprice * (1 - l_discount) else 0 end) / sum(l_extendedprice * (1 - l_discount)) as promo_revenue FROM lineitem,part WHERE l_partkey = p_partkey AND l_shipdate >= date '1995-09-01' AND l_shipdate < date '1995-09-01' + interval '1' month;")
+        self.entry_query.delete("1.0", END)
+        self.entry_query.insert("1.0","SELECT 100.00 * sum(case when p_type like 'PROMO%' then l_extendedprice * (1 - l_discount) else 0 end) / sum(l_extendedprice * (1 - l_discount)) as promo_revenue FROM lineitem,part WHERE l_partkey = p_partkey AND l_shipdate >= date '1995-09-01' AND l_shipdate < date '1995-09-01' + interval '1' month;")
 
     def q19_plan(self):
         self.entry_plan.delete("1.0", END)
-        self.entry_plan.insert("1.0","SELECT sum(l_extendedprice* (1 - l_discount)) as revenue FROM lineitem,part WHERE (p_partkey = l_partkey AND p_brand = 'Brand#12' AND p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG') AND l_quantity >= 1 AND l_quantity <= 1 + 10 AND p_size between 1 AND 5 AND l_shipmode in ('AIR', 'AIR REG') AND l_shipinstruct = 'DELIVER IN PERSON') OR (p_partkey = l_partkey AND p_brand = 'Brand#23' AND p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK') AND l_quantity >= 10 AND l_quantity <= 10 + 10 AND p_size between 1 AND 10 AND l_shipmode in ('AIR', 'AIR REG') AND l_shipinstruct = 'DELIVER IN PERSON') OR (p_partkey = l_partkey AND p_brand = 'Brand#34' AND p_container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG') AND l_quantity >= 20 AND l_quantity <= 20 + 10 AND p_size between 1 AND 15 AND l_shipmode in ('AIR', 'AIR REG') AND l_shipinstruct = 'DELIVER IN PERSON');")
+        self.entry_query.insert("1.0","SELECT sum(l_extendedprice* (1 - l_discount)) as revenue FROM lineitem,part WHERE (p_partkey = l_partkey AND p_brand = 'Brand#12' AND p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG') AND l_quantity >= 1 AND l_quantity <= 1 + 10 AND p_size between 1 AND 5 AND l_shipmode in ('AIR', 'AIR REG') AND l_shipinstruct = 'DELIVER IN PERSON') OR (p_partkey = l_partkey AND p_brand = 'Brand#23' AND p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK') AND l_quantity >= 10 AND l_quantity <= 10 + 10 AND p_size between 1 AND 10 AND l_shipmode in ('AIR', 'AIR REG') AND l_shipinstruct = 'DELIVER IN PERSON') OR (p_partkey = l_partkey AND p_brand = 'Brand#34' AND p_container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG') AND l_quantity >= 20 AND l_quantity <= 20 + 10 AND p_size between 1 AND 15 AND l_shipmode in ('AIR', 'AIR REG') AND l_shipinstruct = 'DELIVER IN PERSON');")
 
     def q3_plan(self):
-        self.entry_plan.delete("1.0", END)
-        self.entry_plan.insert("1.0","SELECT l_orderkey,sum(l_extendedprice * (1 - l_discount)) as revenue,o_orderdate,o_shippriority FROM customer,orders,lineitem WHERE c_mktsegment = 'BUILDING' AND c_custkey = o_custkey AND l_orderkey = o_orderkey AND o_orderdate < date '1995-03-15' AND l_shipdate > date '1995-03-15' GROUP BY l_orderkey,o_orderdate,o_shippriority ORDER BY revenue desc,o_orderdate LIMIT 20;")
+        self.entry_query.delete("1.0", END)
+        self.entry_query.insert("1.0","SELECT l_orderkey,sum(l_extendedprice * (1 - l_discount)) as revenue,o_orderdate,o_shippriority FROM customer,orders,lineitem WHERE c_mktsegment = 'BUILDING' AND c_custkey = o_custkey AND l_orderkey = o_orderkey AND o_orderdate < date '1995-03-15' AND l_shipdate > date '1995-03-15' GROUP BY l_orderkey,o_orderdate,o_shippriority ORDER BY revenue desc,o_orderdate LIMIT 20;")
 
 
     def q5_plan(self):
-        self.entry_plan.delete("1.0", END)
-        self.entry_plan.insert("1.0","SELECT n_name,sum(l_extendedprice * (1 - l_discount)) as revenue FROM customer,orders,lineitem,supplier,nation,region WHERE c_custkey = o_custkey AND l_orderkey = o_orderkey AND l_suppkey = s_suppkey AND c_nationkey = s_nationkey AND s_nationkey = n_nationkey AND n_regionkey = r_regionkey AND r_name = 'ASIA' AND o_orderdate >= date '1994-01-01' AND o_orderdate < date '1994-01-01' + interval '1' year GROUP BY n_name ORDER BY revenue desc;")
+        self.entry_query.delete("1.0", END)
+        self.entry_query.insert("1.0","SELECT n_name,sum(l_extendedprice * (1 - l_discount)) as revenue FROM customer,orders,lineitem,supplier,nation,region WHERE c_custkey = o_custkey AND l_orderkey = o_orderkey AND l_suppkey = s_suppkey AND c_nationkey = s_nationkey AND s_nationkey = n_nationkey AND n_regionkey = r_regionkey AND r_name = 'ASIA' AND o_orderdate >= date '1994-01-01' AND o_orderdate < date '1994-01-01' + interval '1' year GROUP BY n_name ORDER BY revenue desc;")
 
     def q6_plan(self):
-        self.entry_plan.delete("1.0", END)
-        self.entry_plan.insert("1.0","SELECT sum(l_extendedprice * l_discount) as revenue FROM lineitem WHERE l_shipdate >= date '1994-01-01' AND l_shipdate < date '1994-01-01' + interval '1' year AND l_discount between 0.06 - 0.01 AND 0.06 + 0.01 AND l_quantity < 24;")
+        self.entry_query.delete("1.0", END)
+        self.entry_query.insert("1.0","SELECT sum(l_extendedprice * l_discount) as revenue FROM lineitem WHERE l_shipdate >= date '1994-01-01' AND l_shipdate < date '1994-01-01' + interval '1' year AND l_discount between 0.06 - 0.01 AND 0.06 + 0.01 AND l_quantity < 24;")
 
     def explain_query(self):
         """ Explain query """
