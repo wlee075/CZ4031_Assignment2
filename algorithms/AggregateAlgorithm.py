a9f6a23d675cf58d457976b1ec162deac5a25903
wlee075 <wymanleejun@gmail.com>
2021-11-12 14:12:07 +0800

    renamed program to project and removed unused files

--- algorithms/AggregateAlgorithm.py
+++ algorithms/AggregateAlgorithm.py
@@ -11,12 +11,12 @@ def AggregateAlgorithm(queryplan, isStart=False):
         parsedplan += Annotation.get_conjuction()
 
         if "Group Key" in queryplan:
-            parsedplan += "The result is grouped by "
+            parsedplan += "Group result by "
             for group_key in queryplan["Group Key"]:
                 parsedplan += group_key.replace("::text", "") + ", "
             parsedplan = parsedplan[:-2]
         if "Filter" in queryplan:
-            parsedplan += " and bounded with the condition(s) " + queryplan["Filter"].replace("::text", "")
+            parsedplan += " and bind them with the following condition(s) " + queryplan["Filter"].replace("::text", "")
         parsedplan += "."
         return parsedplan
 
@@ -24,13 +24,13 @@ def AggregateAlgorithm(queryplan, isStart=False):
         text = Annotation.get_conjuction()
 
         if len(queryplan["Group Key"]) == 1:
-            text += "It hashes all the rows based on the key "
+            text += "All rows hashed based on the keys "
             text += queryplan["Group Key"][0].replace("::text", "") + ", "
         else:
-            text += "It hashes all the rows based on the keys "
+            text += "All rows hashed based on the keys "
             for i in queryplan["Group Key"]:
                 text += i.replace("::text", "") + ", "
-        text += "then returns the desired row after processing."
+        text += " and after processing, the desired row is returned"
 
         parsedplan = Annotation.parse_plan(queryplan["Plans"][0], isStart)
         parsedplan += " " + text
@@ -39,6 +39,6 @@ def AggregateAlgorithm(queryplan, isStart=False):
     if queryplan["Strategy"] == "Plain":
         parsedplan = Annotation.parse_plan(queryplan["Plans"][0], isStart) + " "
         parsedplan += Annotation.get_conjuction()
-        parsedplan += "The result will be aggregated."
+        parsedplan += "Aggregate the result."
         return parsedplan
 
